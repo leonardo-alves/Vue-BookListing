43d756e7b823e26198852db93872e3686fb1061c
Leonardo Alves da Rosa <leonardo.rosa@4all.com>
2019-11-23 09:59:18 -0300

    :sparkles: add new BookList component

---
+++ src/components/BookList/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BookLis',
+  data() {
+    return {
+      title: 'all books',
+    }
+  }
+}
+</script>
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>

e0ca016a8193f8f3428ae12fde30ade29d5315d2
Leonardo Alves da Rosa <leonardo.rosa@4all.com>
2019-11-23 10:15:16 -0300

    :sparkles: add data for booklist

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
-  name: 'BookList',
+  name: "BookList",
   data() {
     return {
-      title: 'All Books',
-    }
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+      ]
+    };
   }
-}
+};
 </script>
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
+h1,
+h2 {
+  font-weight: normal;
+}
 </style>
